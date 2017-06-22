11ba58d20e3e261f6c175bc9d15e9db42f858602
andrew downie <andrew_dev@outlook.com>
2017-06-21 22:38:31 -0400

    VScode confilct with command line. Completed requirements for real this time.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+
+Extended by Andrew Downie
+
+To run this project:
+    - open main.py in IDLE for python 2, 
+    - with main.py open, select "Run > Run Module" or press F5
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from MovieMetadata import MovieMetadata
+from movie_metadata import movie_metadata
+from fresh_tomatoes import *
 
-testData = MovieMetadata("m-title", "m-boxArt", "m-trailer")
+#In main.py (this file) a list is created to hold movie meta data, and then meta data for three movies are added to this list. 
+#Finally this list is passed to open_movies_page function in the fresh_tomatoes module to add this media data to an html page dynamically.
 
-testData.PrintContents()
+movieList = []
 
+movieList.append( 
+    movie_metadata(
+        "Anchorman: The Legend of Ron Burgundy", 
+        "https://images-na.ssl-images-amazon.com/images/I/51DQR900C0L._SY445_.jpg", 
+        "https://www.youtube.com/watch?v=-T3wnP91OnI"
+    )
+)
+
+movieList.append( 
+    movie_metadata(
+        "Layer Cake", 
+        "https://upload.wikimedia.org/wikipedia/en/e/e8/Layer_Cake_Poster.JPG", 
+        "https://www.youtube.com/watch?v=e5R4iepdXqo"
+    )
+)
+
+movieList.append( 
+    movie_metadata(
+        "Mad Max: Fury Road", 
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUyMTE0ODcxNF5BMl5BanBnXkFtZTgwODE4NDQzNTE@._V1_UY1200_CR97,0,630,1200_AL_.jpg", 
+        "https://www.youtube.com/watch?v=hEJnMQG9ev8"
+    )
+)
+
+open_movies_page(movieList)
