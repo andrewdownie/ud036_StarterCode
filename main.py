87dd62180c59d0d152fad0a501b122a9701a5039
andrew downie <andrew_dev@outlook.com>
2017-06-22 12:16:18 -0400

    main.py and movie_metadata.py now conform to pep8 style requirements.

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 from movie_metadata import movie_metadata
 from fresh_tomatoes import *
 
-#In main.py (this file) a list is created to hold movie meta data, and then meta data for three movies are added to this list. 
-#Finally this list is passed to open_movies_page function in the fresh_tomatoes module to add this media data to an html page dynamically.
+# In main.py (this file):
+#   First: a list is created to hold movie meta data, and then meta data for
+#          movies are added to this list.
+#   Finally: this list is passed to open_movies_page function in the
+#            fresh_tomatoes module, the open_movie_page function  then creates
+#            the html page.
 
 movieList = []
 
-movieList.append( 
+movieList.append(
     movie_metadata(
-        "Anchorman: The Legend of Ron Burgundy", 
-        "https://images-na.ssl-images-amazon.com/images/I/51DQR900C0L._SY445_.jpg", 
+        "Anchorman: The Legend of Ron Burgundy",
+        "http://www.vhsisland.com/wp-content/uploads/2013/09/anchorman.jpg",
         "https://www.youtube.com/watch?v=-T3wnP91OnI"
     )
 )
 
-movieList.append( 
+movieList.append(
     movie_metadata(
-        "Layer Cake", 
-        "https://upload.wikimedia.org/wikipedia/en/e/e8/Layer_Cake_Poster.JPG", 
+        "Layer Cake",
+        "https://upload.wikimedia.org/wikipedia/en/e/e8/Layer_Cake_Poster.JPG",
         "https://www.youtube.com/watch?v=e5R4iepdXqo"
     )
 )
 
-movieList.append( 
+movieList.append(
     movie_metadata(
-        "Mad Max: Fury Road", 
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUyMTE0ODcxNF5BMl5BanBnXkFtZTgwODE4NDQzNTE@._V1_UY1200_CR97,0,630,1200_AL_.jpg", 
+        "Mad Max: Fury Road",
+        "http://e.snmc.io/lk/f/l/98ef95be769a877de5a4cc6204a06866/5700650.jpg",
         "https://www.youtube.com/watch?v=hEJnMQG9ev8"
     )
 )
--- movie_metadata.py
+++ movie_metadata.py
@@ -1,23 +1,22 @@
-#This class holds the 3 required pieces of meta data for a movie: 
+# This class holds the 3 required pieces of meta data for a movie:
 #   - the title of the movie
 #   - url of the movies poster image
 #   - the url of a trailer for the movie on youtube
+
+
 class movie_metadata(object):
     title = '<BLANK>'
     poster_image_url = '<BLANK>'
     trailer_youtube_url = '<BLANK>'
 
-
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
-        self.poster_image_url = poster_image_url 
+        self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-    
 
     def PrintAll(self):
-    #Prints all of the instance variables of this class to the command line.
+        # Prints all of the variables of this class to the command line.
         print("Movie info:")
         print("\t" + self.title)
         print("\t" + self.poster_image_url)
         print("\t" + self.trailer_youtube_url)
-
